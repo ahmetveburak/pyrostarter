df5b165f0d5550eb54138aa501bc874a7e173152
Astrapel Lygdamus <astrapel@protonmail.ch>
2020-12-22 01:54:14 +0300

    contents triple quote and more readable template

--- pyrostarter/contents.py
+++ pyrostarter/contents.py
@@ -1,7 +1,102 @@
 phrases = {
-    "main": "from MODULE_NAME.BotConfig import BOT_NAME\n\nif __name__ == '__main__':\n    BOT_NAME().run()",
-    "config": "[pyrogram]\nAPI_ID = api_id\nAPI_HASH = api_hash\nBOT_TOKEN = bot_token",
-    "botconfig": 'from pyrogram import Client\nfrom pyrogram.types import Message\n\n\nclass BOT_NAME(Client, Message):\n    def __init__(self):\n        moduleName = "MODULE_NAME"\n        name = self.__class__.__name__.lower()\n        super().__init__(\n            session_name=name,\n            config_file=f"{moduleName}/{name}.ini",\n            workers=8,\n            plugins=dict(root=f"{moduleName}/plugins"),\n        )\n\n    async def start(self):\n        await super().start()\n\n    async def stop(self, *args):\n        await super().stop()\n',
-    "plugin": 'from pyrogram import Client, filters\nfrom pyrogram.types import Message, CallbackQuery\nfrom functools import partial\nfrom MODULE_NAME.BotConfig import BOT_NAME\nfrom MODULE_NAME.utils import buttonator\nimport asyncio, time\n\ncommand = partial(filters.command, prefixes=["!", "/", "."])\n\n\n@BOT_NAME.on_message(command("start"))\nasync def say_hello(client: Client, message: Message) -> None:\n    msg = await message.reply_text(text="`Hello..`", quote=True)\n    await asyncio.sleep(1)\n    await msg.edit_text(text="`Hello I\'m Ready to Use`")\n\n\n@BOT_NAME.on_message(command("ping"))\nasync def ping(_, message: Message):\n    start = time.time()\n    reply = await message.reply_text("...")\n    delta_ping = time.time() - start\n    await reply.edit_text(f"**Pong!** `{delta_ping * 1000:.3f} ms`")\n\n\nmy_buttons = {\n    "Button1": "Button1",\n    "Button2": "Button2",\n    "Button3": "Button3",\n    "Button4": "Button4",\n    "Button5": "Button5",\n}\nbutton_filter = filters.create(lambda _, __, query: query.data in my_buttons.keys())\n\n\n@BOT_NAME.on_message(command("buttons"))\nasync def buttons(client: Client, message: Message):\n    me = await client.get_me()\n\n    if me.is_bot:\n        await message.reply_text(\n            text="These are test buttons with callback",\n            reply_markup=buttonator.button_maker(buttons=my_buttons, size=2),\n        )\n\n    else:\n        await message.reply_text(\n            text="Users can not send `keyboard markup`. **I\'m sorry..**",\n        )\n\n\n@BOT_NAME.on_callback_query(button_filter)\nasync def reply_callback(_: Client, callback=CallbackQuery):\n    await callback.message.reply_text(text=callback.data)\n',
-    "util": "from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup\n\n\ndef slicer(button_list: list, size: int) -> list:\n    return [button_list[i : i + size] for i in range(0, len(button_list), size)]\n\n\ndef button_maker(buttons: dict, size: int) -> InlineKeyboardMarkup:\n    buttons = [\n        InlineKeyboardButton(text=v, callback_data=k) for k, v in buttons.items()\n    ]\n    buttons = slicer(buttons, size)\n\n    return InlineKeyboardMarkup(buttons)\n",
+    "main": """
+    from MODULE_NAME.BotConfig import BOT_NAME
+    
+    if __name__ == '__main__':
+        BOT_NAME().run()
+
+    """,
+
+    "config": """
+    [pyrogram]
+    API_ID = api_id
+    API_HASH = api_hash
+    BOT_TOKEN = bot_token
+    
+    """,
+
+    "botconfig": """
+    
+    from pyrogram import Client
+    from pyrogram.types import Message
+    
+    
+    class BOT_NAME(Client, Message):
+        def __init__(self):
+            moduleName = "MODULE_NAME"
+            name = self.__class__.__name__.lower()
+            super().__init__(session_name=name,config_file=f"{moduleName}/{name}.ini",workers=8,plugins=dict(root=f"{moduleName}/plugins"),)
+        
+        async def start(self):
+            await super().start()
+
+        async def stop(self, *args):
+            await super().stop()
+    """,
+
+    "plugin": """
+
+    from pyrogram import Client, filters
+    from pyrogram.types import Message, CallbackQuery
+    from functools import partial
+    from MODULE_NAME.BotConfig import BOT_NAME
+    from MODULE_NAME.utils import buttonator
+    import asyncio, time
+
+    command = partial(filters.command, prefixes=["!", "/", "."])
+
+    @BOT_NAME.on_message(command("start"))
+    async def say_hello(client: Client, message: Message) -> None:
+        msg = await message.reply_text(text="`Hello..`", quote=True)
+        await asyncio.sleep(1)
+        await msg.edit_text(text="`Hello I\'m Ready to Use`")
+    
+
+    @BOT_NAME.on_message(command("ping"))
+    async def ping(_, message: Message):
+        start = time.time()
+        reply = await message.reply_text("...")
+        delta_ping = time.time() - start
+        await reply.edit_text(f"**Pong!** `{delta_ping * 1000:.3f} ms`")
+        my_buttons = {"Button1": "Button1","Button2": "Button2","Button3": "Button3", "Button4": "Button4",    "Button5": "Button5",}
+        button_filter = filters.create(lambda _, __, query: query.data in my_buttons.keys())
+            
+            
+    @BOT_NAME.on_message(command("buttons"))
+    async def buttons(client: Client, message: Message):    
+        me = await client.get_me()
+
+            if me.is_bot:        
+                await message.reply_text(text="These are test buttons with callback",reply_markup=buttonator.button_maker(buttons=my_buttons, size=2),)
+                
+            else:        
+                await message.reply_text(
+                    text="Users can not send `keyboard markup`. **I\'m sorry..**",)
+
+    @BOT_NAME.on_callback_query(button_filter)
+    async def reply_callback(_: Client, callback=CallbackQuery):    
+        await callback.message.reply_text(text=callback.data)
+    
+    """,
+    
+    
+    
+    
+    "util": """
+
+    from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+    def slicer(button_list: list, size: int) -> list:
+        return [button_list[i : i + size] for i in range(0, len(button_list), size)]
+
+
+    def button_maker(buttons: dict, size: int) -> InlineKeyboardMarkup:
+        buttons = [InlineKeyboardButton(text=v, callback_data=k) for k, v in buttons.items()
+        
+        buttons = slicer(buttons, size)
+        
+        return InlineKeyboardMarkup(buttons)
+        
+
+    """,
 }
