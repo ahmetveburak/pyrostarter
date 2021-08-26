abe893b7507cc0ce90ec53dfada33ea0fa3a7df8
ahmetveburak <ahmetbozyurtt@gmail.com>
2021-08-26 16:33:16 +0300

    #7 interactive init
    interactive template initializer created with 'cleo' package. dependencies updated.

---
+++ pyrostarter/commands/init.py
@@ -0,0 +1,131 @@
+from typing import Optional
+
+from cleo import Command
+from cleo.helpers import option
+from pyrostarter.constructor import builder
+
+
+class InitCommand(Command):
+    name = "init"
+    description = "Creates Telegram Bot template structure."
+
+    options = [
+        option("project-name", description="Name of the bot project.", flag=False),
+        option("bot-name", description="Name of the bot.", flag=False),
+        option("api-id", description="Telegram API ID.", flag=False),
+        option("api-hash", description="Telegram API Hash.", flag=False),
+        option("bot-token", description="Bot Token.", flag=False),
+        option("userbot", description="Is userbot?", flag=False),
+        option(
+            "venv-type",
+            description="Standart/Poetry?, default: no virtual environment",
+            flag=False,
+        ),
+    ]
+
+    help = "The init command starts the interactive bot setup."
+
+    def __init__(self):
+
+        super(InitCommand, self).__init__()
+
+    def handle(self) -> Optional[int]:
+        self.add_style("opt", fg="red", options=["bold"])
+        if self.io.is_interactive():
+            self.line("")
+            self.line("This command will guide you through creating your <info>Telegram Bot</> template.")
+            self.line("")
+
+        project_name = self.option("project-name")
+        if not project_name:
+            question = self.create_question("Project name:")
+            project_name = self.ask(question)
+            while not self.check_answer(project_name):
+                project_name = self.ask(question)
+
+        bot_name = self.option("bot-name")
+        if not bot_name:
+            question = self.create_question("Bot name:")
+            bot_name = self.ask(question)
+            while not self.check_answer(bot_name):
+                bot_name = self.ask(question)
+
+        self.line(
+            "\n<comment>API ID, API Hash and Bot Token are optional.\n"
+            "You can enter manually to</comment> <info>.ini</info> <comment>file after setup.</comment>\n"
+        )
+
+        api_id = self.option("api-id")
+        if not api_id:
+            api_id = ""
+            question = self.create_question("Your API ID:")
+            api_id = self.ask(question)
+
+        api_hash = self.option("api-hash")
+        if not api_hash:
+            api_hash = ""
+            question = self.create_question("Your API Hash:")
+            api_hash = self.ask(question)
+
+        bot_token = self.option("bot-token")
+        if not bot_token:
+            bot_token = ""
+            question = self.create_question("Your Bot Token:")
+            bot_token = self.ask(question)
+
+        userbot = self.option("userbot")
+        if not userbot:
+            userbot = False
+            question = self.create_question(f"Is user bot?:", default=userbot, type="confirmation")
+            userbot = self.ask(question)
+
+        venv_type = self.option("venv-type")
+        if not venv_type:
+            venv_type = "no"
+            self.line("<comment>\nVirtual enviroment type? Default: No virtual environment.</>\n")
+            question = self.create_question("Standart, Poetry (s/p):", default=venv_type)
+            venv_type = self.ask(question).lower()
+            print(venv_type)
+
+        venvs = {"s": "Standart", "p": "Poetry"}
+
+        self.line(
+            f"<info>Project Name  :</> <comment>{project_name}\n</>"
+            f"<info>Bot Name      :</> <comment>{bot_name}\n</>"
+            f"<info>Userbot       :</> <comment>{userbot}\n</>"
+            f"<info>Venv Type     :</> <comment>{venvs.get(venv_type, 'No venv.')}\n</>"
+            f"<info>API_ID        :</> <comment>{'Provided.' if api_id else 'Not provided.'}\n</>"
+            f"<info>API_HASH      :</> <comment>{'Provided.' if api_hash else 'Not provided.'}\n</>"
+            f"<info>BOT_TOKEN     :</> <comment>{'Provided.' if bot_token else 'Not provided.'}\n</>"
+        )
+
+        question = self.create_question("Do you confirm generation?:", default=True, type="confirmation")
+        confirm = self.ask(question)
+
+        if not confirm:
+            self.line("<error>Aborted..</>")
+            return
+
+        self.line("<error>Not Aborted..</>")
+
+        builder(
+            project_name=project_name,
+            bot_name=bot_name,
+            api_id=api_id,
+            api_hash=api_hash,
+            bot_token=bot_token if userbot else "",
+            venv_type=venv_type,
+        )
+
+    def check_answer(self, answer: str) -> bool:
+        if not answer:
+            self.line("<error>This field is required.</error>\n")
+            return False
+        elif " " in answer:
+            self.line("<error>No whitespace allowed in this field.</error>\n")
+            return False
+        elif answer[0].isdigit():
+            self.line("<error>The field name cannot start with digits.</error>\n")
+            return False
+        else:
+            return True
--- pyrostarter/constructor.py
+++ pyrostarter/constructor.py
@@ -3,7 +3,7 @@ import os
 from pyrostarter.contents import phrases
 
 
-def virtualenv_type(repo_name: str):
+def virtualenv_type() -> None:
     try:
         from venv import EnvBuilder
 
@@ -13,22 +13,21 @@ def virtualenv_type(repo_name: str):
             symlinks=False,
             with_pip=True,
         )
-        venv_manager.create(f"{repo_name}/.venv")
+        venv_manager.create(".venv")
 
-        with open(f"{repo_name}/requirements.txt", "w") as f:
+        with open(f"requirements.txt", "w") as f:
             f.write(phrases["requirements"])
 
     except ModuleNotFoundError:
-        print("venv module not found. install and create yourself manually\n")
+        print("venv module not found. you can install and create yourself manually\n")
 
 
-def poetry_type(repo_name: str, project_name: str):
-    f = open(f"{repo_name}/pyproject.toml", "w")
+def poetry_type(project_name: str) -> None:
+    f = open(f"pyproject.toml", "w")
     f.write(phrases["poetry"].replace("MODULE_NAME", project_name))
 
 
-def setup(
-    repo_name: str,
+def builder(
     project_name: str,
     bot_name: str,
     api_id: str = "",
@@ -37,12 +36,12 @@ def setup(
     venv_type: str = "none",
 ) -> None:
 
-    os.makedirs(f"{repo_name}/{project_name}/plugins")
-    os.makedirs(f"{repo_name}/{project_name}/utils")
+    os.makedirs(f"{project_name}/plugins")
+    os.makedirs(f"{project_name}/utils")
 
     venv_dict = {
-        "virtualenv": virtualenv_type(repo_name=repo_name),
-        "poetry": poetry_type(repo_name=repo_name, project_name=project_name),
+        "virtualenv": virtualenv_type(),
+        "poetry": poetry_type(project_name=project_name),
     }
 
     venv_dict.get(venv_type)
@@ -50,18 +49,18 @@ def setup(
     file_list: list = ["/__main__.py", "/BotConfig.py", "/plugins/say_hello.py"]
     file_phrases: list = ["main", "botconfig", "plugin"]
 
-    with open(f"{repo_name}/{project_name}/__init__.py", "w") as f:
+    with open(f"{project_name}/__init__.py", "w") as f:
         f.write('__version__ = "0.1.0"')
 
     for file, phrase in zip(file_list, file_phrases):
-        open(f"{repo_name}/{project_name}{file}", "w").write(
+        open(f"{project_name}{file}", "w").write(
             phrases[phrase].replace("BOT_NAME", bot_name).replace("MODULE_NAME", project_name)
         )
 
-    with open(f"{repo_name}/{project_name}/utils/buttonator.py", "w") as f:
+    with open(f"{project_name}/utils/buttonator.py", "w") as f:
         f.write(phrases["util"])
 
-    with open(f"{repo_name}/{project_name}/{bot_name.lower()}.ini", "w") as f:
+    with open(f"{project_name}/{bot_name.lower()}.ini", "w") as f:
         f.write(
             phrases["config"].replace("api_id", api_id).replace("api_hash", api_hash).replace("bot_token", bot_token)
         )
