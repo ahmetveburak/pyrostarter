38e7f6f02c1d0321737cce4c1052529ecb18f912
ahmetveburak <ahmetbozyurtt@gmail.com>
2022-11-06 12:56:25 +0300

    refactor with black & isort

--- pyrostarter/constructor.py
+++ pyrostarter/constructor.py
@@ -3,11 +3,7 @@ import os
 from pyrostarter.contents import phrases
 
 
-def none_venv(none) -> None:
-    pass
-
-
-def virtualenv_type(none) -> None:
+def builtin_venv() -> None:
     try:
         from venv import EnvBuilder
 
@@ -26,7 +22,7 @@ def virtualenv_type(none) -> None:
         print("venv module not found. you can install and create yourself manually\n")
 
 
-def poetry_type(project_name: str) -> None:
+def poetry_venv(project_name: str) -> None:
     f = open(f"pyproject.toml", "w")
     f.write(phrases["poetry"].replace("MODULE_NAME", project_name))
 
@@ -37,18 +33,16 @@ def builder(
     api_id: str = "",
     api_hash: str = "",
     bot_token: str = "",
-    venv_type: str = "none",
+    venv_type: str = "",
 ) -> None:
 
     os.makedirs(f"{project_name}/plugins")
     os.makedirs(f"{project_name}/utils")
 
-    venv_dict = {
-        "Standart": virtualenv_type,
-        "Poetry": poetry_type,
-    }
-
-    venv_dict.get(venv_type, none_venv)(project_name)
+    if venv_type == "Poetry":
+        poetry_venv(project_name)
+    else:
+        builtin_venv()
 
     file_list: list = ["/__main__.py", "/BotConfig.py", "/plugins/say_hello.py"]
     file_phrases: list = ["main", "botconfig", "plugin"]
@@ -58,7 +52,9 @@ def builder(
 
     for file, phrase in zip(file_list, file_phrases):
         open(f"{project_name}{file}", "w").write(
-            phrases[phrase].replace("BOT_NAME", bot_name).replace("MODULE_NAME", project_name)
+            phrases[phrase]
+            .replace("BOT_NAME", bot_name)
+            .replace("MODULE_NAME", project_name)
         )
 
     with open(f"{project_name}/utils/buttonator.py", "w") as f:
@@ -66,5 +62,8 @@ def builder(
 
     with open(f"{bot_name.lower()}.ini", "w") as f:
         f.write(
-            phrases["config"].replace("api_id", api_id).replace("api_hash", api_hash).replace("bot_token", bot_token)
+            phrases["config"]
+            .replace("api_id", api_id)
+            .replace("api_hash", api_hash)
+            .replace("bot_token", bot_token)
         )
